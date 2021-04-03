8b8a72efcbbaafa80072fe7414fa620272edc4c2
Otmane-Guo <58006515+Otmane-Guo@users.noreply.github.com>
2021-04-03 23:28:02 +0100

    Update test.tf

--- test.tf
+++ test.tf
@@ -2,3 +2,8 @@ resource "local_file" "test" {
     filename = "/home/ops/Bureau/grilo-cli/test.txt"
     content = "this is a test file"  
   }
+
+
+output "cluster_url" {
+    value = "https://invalid-link.com"
+}
